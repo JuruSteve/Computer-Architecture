6d02ea2741956d8e184b81beb82bd1ced95cb05b
jurusteve <jstevemu17@gmail.com>
2019-11-11 19:08:56 -0500

    first commit

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,8 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.register = [0]*256
+        self.PC = 0
 
     def load(self):
         """Load a program into memory."""

375cf8db5d962dd212a9b9d5e039215973a3b552
jurusteve <jstevemu17@gmail.com>
2019-12-16 19:53:07 -0500

    completed day1 project

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,10 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0]*256
+        self.reg = [0]*8
+        self.pc = 0
+        self.instructions = {'HLT':0b00000001, 'LDI': 0b10000010, 'PRN': 0b01000111, "ADD": 0b10100000, "MUL": 0b10100010, "PUSH": 0b01000101, "POP": 0b01000110} 
 
     def load(self):
         """Load a program into memory."""
@@ -39,7 +42,13 @@ class CPU:
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
-
+    
+    def ram_read(self, adr_to_read):
+        return self.ram[adr_to_read]
+    
+    def ram_write(self, adr_to_write, value):
+        self.ram[adr_to_write] = value
+        # return self.ram[adr_to_write]
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
@@ -62,4 +71,21 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        halted = False
+
+        while not halted:
+            Ir = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            if Ir == self.instructions['HLT']:
+                halted = True
+            elif Ir == self.instructions['LDI']:
+                self.reg[operand_a] = operand_b
+                self.pc +=3
+            elif Ir == self.instructions['PRN']:
+                val = self.reg[operand_a]
+                self.pc +=2
+                print(val)
+            else:
+                print('Unknown instruction')
+                sys.exit(1)
